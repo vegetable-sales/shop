af66df929e9261ff4ee1361c4bbb262faaa14fbe
rockdurai <duraikalai9787@gmail.com>
2024-08-27 12:10:49 +0530

    first

---
+++ login/login.js
@@ -0,0 +1,12 @@
+const feedbackform = document.getElementById('feadbackform')
+const responsivdiv = document.getElementById('responce')
+
+console.log(feedbackform);
+
+feedbackform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
+
+
+console.log(form);
+
